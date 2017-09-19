3540727c9b05ae17cefeecc2c1f87469441b9fc0
Ondrej Biza <ondrej.biza@gmail.com>
2017-09-19 23:15:10 +0900

    http based communication added

--- server.py
+++ server.py
@@ -1,122 +1,117 @@
-import json, os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import multiprocessing, urllib
 
-class TFServer:
+class SessionServer(HTTPServer):
 
-  FILE = "session.json"
-  MODIFIABLE_PARAMETERS = "modifiable_parameters"
-  STATIC_PARAMETERS = "static_parameters"
-  RECEIVED = "received"
+  def __init__(self, tensors, session, address="127.0.0.1", port=8000):
 
-  def __init__(self, path, overwrite=True):
+    super(HTTPServer, self).__init__((address, port), self.RequestHandler)
 
-    self.path = path
-    self.modifiable_parameters = None
-    self.static_parameters = None
-    self.session = None
+    manager = multiprocessing.Manager()
+    self.shared = manager.dict()
 
-    if not os.path.exists(path):
-      os.makedirs(path)
-    elif not os.path.isdir(path):
-      raise ValueError("{:s} is not a directory.".format(path))
+    self.shared["tensors"] = tensors
+    self.shared["session"] = session
+    self.shared["tensor_names"] = [tensor.name for tensor in tensors]
 
-    self.file = os.path.join(path, self.FILE)
+    self.shared["events"] = manager.list()
+    self.shared["past_events"] = manager.list()
 
-    if os.path.exists(self.file):
+    print("init")
 
-      if overwrite:
-        os.remove(self.file)
+  class RequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+      length = int(self.headers['Content-Length'])
+      post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
+
+      error = False
+      iteration = None
+      tensor_name = None
+      value = None
+
+      if "iteration" in post_data and "tensor_name" in post_data and "value" in post_data:
+
+        try:
+          iteration = int(post_data["iteration"][0])
+        except ValueError:
+          error = True
+
+        tensor_name = post_data["tensor_name"][0]
+        value = post_data["value"][0]
+
+        if "value_type" in post_data:
+          value_type = post_data["value_type"][0]
+
+          if value_type == "int":
+            value = int(value)
+          elif value_type == "float":
+            value = float(value)
+          elif value_type == "string":
+            pass
+          else:
+            error = True
       else:
-        raise ValueError("{:s} already exists. Either choose a different directory or set overwrite to True."
-                         .format(self.file))
+        error = True
 
-  def register_session(self, modifiable_parameters, static_parameters, session):
 
-    if self.session is not None:
-      raise ValueError("A session is already registered.")
+      if not error:
+        self.add_event(iteration, tensor_name, value)
+        self.send_response_only(200)
+      else:
+        self.send_error(400, "Invalid add event request.")
 
-    self.modifiable_parameters = modifiable_parameters
-    self.static_parameters = static_parameters
-    self.session = session
+      self.end_headers()
 
-    self.__save_params()
+    def do_DELETE(self):
+      length = int(self.headers['Content-Length'])
+      post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
 
-  def check_update(self):
+      error = False
+      event_idx = None
 
-    params = self.__load_params()
-    params = self.__map_names_to_params(params, self.modifiable_parameters)
+      if "event_idx" in post_data:
+        event_idx = int(post_data["event_idx"][0])
 
-    changed = self.__find_changes(self.modifiable_parameters, params)
+        if event_idx >= len(self.server.shared["events"]):
+          error = True
+      else:
+        error = True
 
-    if len(changed.keys()) > 0:
-      print("parameter changes:")
+      if not error:
+        del self.server.shared["events"][event_idx]
+        self.send_response_only(200)
+      else:
+        self.send_error(400, "Invalid delete event request.")
 
-      for key, val in changed.items():
-        print("{:s}: {}".format(key.name, val))
-        self.session.run(key.assign(val))
+      self.end_headers()
 
-      self.modifiable_parameters = params
-      self.__save_params()
+    def add_event(self, iteration, tensor_name, value):
+      self.server.shared["events"].append(Event(iteration, tensor_name, value))
 
-  @staticmethod
-  def __map_params_to_names(params):
+  def check_events(self, iteration):
 
-    names = {}
+    for idx, event in enumerate(reversed(self.shared["events"])):
 
-    for key, item in params.items():
-      names[key.name] = item
+      if event.iteration <= iteration:
+        self.assign_value(event.tensor_name, event.value)
+        del self.shared["events"][idx]
 
-    return names
+  def assign_value(self, tensor_name, value):
 
-  @staticmethod
-  def __map_names_to_params(names, params):
+    tensor_index = self.shared["tensor_names"].index(tensor_name)
+    tensor = self.shared["tensors"][tensor_index]
+    self.shared["session"].run(tensor.assign(value))
 
-    new_params = {}
+class Event:
 
-    for key, item in names.items():
+  def __init__(self, iteration, tensor_name, value):
 
-      param = None
-      for key2, item2 in params.items():
-        if key2.name == key:
-          param = key2
+    self.iteration = iteration
+    self.tensor_name = tensor_name
+    self.value = value
 
-      assert param is not None
 
-      new_params[param] = item
-
-    return new_params
-
-  @staticmethod
-  def __find_changes(old_params, new_params):
-
-    changed = {}
-
-    for key, new_val in new_params.items():
-
-      old_val = old_params[key]
-
-      if old_val != new_val:
-        changed[key] = new_val
-
-    return changed
-
-  def __save_params(self):
-
-    obj = {
-      self.MODIFIABLE_PARAMETERS: self.__map_params_to_names(self.modifiable_parameters),
-      self.STATIC_PARAMETERS: self.__map_params_to_names(self.static_parameters),
-      self.RECEIVED: True
-    }
-
-    with open(self.file, "w") as file:
-      json.dump(obj, file)
-
-  def __load_params(self):
-
-    try:
-      with open(self.file, "r") as file:
-        obj = json.load(file)
-    except FileNotFoundError:
-      print("Session file not accessible, skipping update.")
-      return self.modifiable_parameters
-
-    return obj[self.MODIFIABLE_PARAMETERS]
+httpd = SessionServer([], None)
+print("Running server.")
+httpd.serve_forever()
