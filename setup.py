167377f7eb05551483fcbb5d18347f4f7a26201f
Ondrej Biza <ondrej.biza@gmail.com>
2018-01-05 23:13:29 +0100

    improved setup

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 from setuptools import setup
+
+def readme():
+  with open('README.rst') as f:
+    return f.read()
+
 setup(
   name="tfset",
   version="1.0",
-  describe="Set Tensor values during training in Tensorflow.",
-  url="https://github.com/ondrejba/interactive-tensorflow",
+  description="Set Tensor values during training in Tensorflow.",
+  long_description=readme(),
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence"
+  ],
+  keywords="tensorflow interactive",
+  url="https://github.com/ondrejba/tfset",
   author="Ondrej Biza",
   author_email="ondrej.biza@gmail.com",
   license="MIT",
   packages=["tfset"],
   install_requires=[
     "tensorflow"
-  ]
+  ],
+  include_package_data=True,
+  zip_safe=False
 )
